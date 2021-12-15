3ae38c7e327f3311daf7a72ee1648b4d4dfd7615
PauloHenriqueMagno <paulohm2309@gmail.com>
2021-12-15 13:32:05 -0300

    feat: add routes

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-# Código do dev aqui
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def home():
+    return {
+      "data": 'Hello Flask!'
+    }
+
+@app.route('/current_datetime', methods=["GET"])
+def time():
+    get_date = datetime.now()
+    date = get_date.strftime('%d/%m/%Y %T %p')
+    hour = get_date.strftime('%H')
+    hour = int(hour)
+
+    if hour < 12:
+        msg = "Bom dia!"
+    elif hour < 18:
+        msg = "Boa tarde!"
+    else:
+        msg = "Boa noite!"
+
+    return {
+      "current_datetime": date,
+      "message": msg
+    }
